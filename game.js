fdd60bd497a3a786cbe578860d52f22d83df95a6
Reginaldo Inácio <100529825+ReginaldoInacio@users.noreply.github.com>
2025-01-27 20:16:10 -0300

    Update game.js

--- game.js
+++ game.js
@@ -38,7 +38,7 @@ getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        return window.location.assign('educahans2/end');
+        return window.location.assign('/educahans2/end');
     }
     questionCounter++;
     progressText.innerText = `Pergunta ${questionCounter}/${MAX_QUESTIONS}`;
