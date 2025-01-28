2d9fc2d92f3ed59a35ecd62db0864d072a4c72de
Reginaldo Inácio <100529825+ReginaldoInacio@users.noreply.github.com>
2025-01-27 21:36:43 -0300

    Update end.js

--- end.js
+++ end.js
@@ -25,10 +25,10 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-   // window.location.assign('/');
+    window.location.assign('/educahans2/index.html');
     // Exibe uma mensagem de confirmação
   alert("Pontuação salva com sucesso!");
-
+/*
   // Redireciona para a página inicial
-  window.location.assign("/educahans2/index.html");
+  window.location.assign("/educahans2/index.html");*/
 };
